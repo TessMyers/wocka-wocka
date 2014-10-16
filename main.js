9692d87af28c05fb38c3ea127558fb9c2e15e34d
Tess Myers <tess@tessmyers.com>
2014-10-15 19:47:10 -0700

    adds text element to click handler

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   var audioElement = document.createElement('audio');
     audioElement.setAttribute('src', 'solo.mp3');
 
-  $('.sax').on('click', function(){
+  $('.sax').add('.text').on('click', function(){
     console.log('playing');
     audioElement.play();
   })
